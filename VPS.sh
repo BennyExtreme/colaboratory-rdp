23b112d26475ea58504197aeb7f532053f35ed5d
BennyExtreme <75805898+BennyExtreme@users.noreply.github.com>
2021-03-23 15:46:27 +0100

    Create VPS.sh

---
+++ VPS.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+printf "Installing RDP Be Patience... " >&2
+{
+sudo useradd -m BennyExtreme
+sudo adduser BennyExtreme sudo
+echo 'BennyExtreme:admin' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo adduser BennyExtreme chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Complete \nCreated new user BennyExtreme with password: admin \nYou can change both BennyExtreme's password with passwd(with no sudo) \nOr you can change root's password with sudo passwd" >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - BennyExtreme -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+else
+    printf "\n\nError Occured " >&2
+fi
